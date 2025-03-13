7353c3641d09c8c59983e45b1bcd0715cd8f44b3
Daniel Motz <hello@danielmotz.de>
2025-03-13 16:50:18 +0100

    move mockup nodes to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
+import {
+  Node,
+  Edge
+} from '@xyflow/react';
+
+
 const longTreeTemplate = {
   root: {
     "LexerRegeln.g4": null,
@@ -324,11 +330,11 @@ WORD : [a-zA-Z][a-zA-Z0-9]* ;
 SPECIAL : [;:'"\\-_()?!/=+*<>] ;` },
 ];
 
-const initialNodes = [
+const initialNodes:Node[] = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges:Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
 
 const nodeWidth = 172;
 const nodeHeight = 36;
