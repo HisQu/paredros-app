ebc18a62662262fb1647172089cd3e3a5d25c08e
Daniel Motz <hello@danielmotz.de>
2025-06-02 17:32:11 +0200

    Remove python script wrapper

--- src-tauri/paredros-debugger/get_parse_info.py
+++
@@ -1,9 +0,0 @@
-from paredros_debugger.ParseInformation import ParseInformation
-
-def get_parse_info(grammar_file_path: str) -> ParseInformation:
-    parse_info = ParseInformation(grammar_file_path)
-    return parse_info
-
-
-if __name__ == "__main__":
-    get_parse_info("../../examples/Regest/Regest.g4", "../../examples/Regest/input.txt")
