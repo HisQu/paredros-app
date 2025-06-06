42264b99e93a63b24e5bc6346ec3b73e83375349
Friedrich Answin Daniel Motz <hello@danielmotz.de>
2025-06-06 15:24:04 +0200

    Add python-src as a startpoint for pyinstaller with script wrapper
    - Rust remains unchanged
    - only pyinstaller needs a script as an entry point to build the dependencies
    - add tauri sidecar `paredros-py`

---
+++ src-python/__main__.py
@@ -0,0 +1,8 @@
+from paredros_debugger.ParseInformation import ParseInformation
+
+def get_parse_info(grammar_file_path: str) -> ParseInformation:
+    parse_info = ParseInformation(grammar_file_path)
+    return parse_info
+
+if __name__ == "__main__":
+    p = get_parse_info("examples/Regest/Regest.g4")
